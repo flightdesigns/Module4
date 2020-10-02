dd9de9d5479bff037963550897c422729f71c019
flightdesigns <70185280+flightdesigns@users.noreply.github.com>
2020-10-01 22:07:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// *******************************
+(function (window) {
+*******************************
 // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
 //
